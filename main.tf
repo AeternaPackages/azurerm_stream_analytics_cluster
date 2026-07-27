ee05ba9165521c33ada32d48550d893f3989c972
Daniel Allen <dan@aeternagroup.net>
2026-07-27 18:31:11 +0100

    Update to schema version v4.81.0

--- main.tf
+++ main.tf
@@ -241,138 +241,138 @@ locals {
 }
 
 module "stream_analytics_clusters" {
-  source                    = "git::https://github.com/AeternaModules/azurerm_stream_analytics_cluster.git?ref=v4.80.0"
+  source                    = "git::https://github.com/AeternaModules/azurerm_stream_analytics_cluster.git?ref=v4.81.0"
   stream_analytics_clusters = local.stream_analytics_clusters
 }
 
 module "stream_analytics_jobs" {
-  source                = "git::https://github.com/AeternaModules/azurerm_stream_analytics_job.git?ref=v4.80.0"
+  source                = "git::https://github.com/AeternaModules/azurerm_stream_analytics_job.git?ref=v4.81.0"
   stream_analytics_jobs = local.stream_analytics_jobs
   depends_on            = [module.stream_analytics_clusters]
 }
 
 module "stream_analytics_managed_private_endpoints" {
-  source                                     = "git::https://github.com/AeternaModules/azurerm_stream_analytics_managed_private_endpoint.git?ref=v4.80.0"
+  source                                     = "git::https://github.com/AeternaModules/azurerm_stream_analytics_managed_private_endpoint.git?ref=v4.81.0"
   stream_analytics_managed_private_endpoints = local.stream_analytics_managed_private_endpoints
   depends_on                                 = [module.stream_analytics_clusters]
 }
 
 module "stream_analytics_function_javascript_uda" {
-  source                                   = "git::https://github.com/AeternaModules/azurerm_stream_analytics_function_javascript_uda.git?ref=v4.80.0"
+  source                                   = "git::https://github.com/AeternaModules/azurerm_stream_analytics_function_javascript_uda.git?ref=v4.81.0"
   stream_analytics_function_javascript_uda = local.stream_analytics_function_javascript_uda
   depends_on                               = [module.stream_analytics_jobs]
 }
 
 module "stream_analytics_function_javascript_udfs" {
-  source                                    = "git::https://github.com/AeternaModules/azurerm_stream_analytics_function_javascript_udf.git?ref=v4.80.0"
+  source                                    = "git::https://github.com/AeternaModules/azurerm_stream_analytics_function_javascript_udf.git?ref=v4.81.0"
   stream_analytics_function_javascript_udfs = local.stream_analytics_function_javascript_udfs
   depends_on                                = [module.stream_analytics_jobs]
 }
 
 module "stream_analytics_job_schedules" {
-  source                         = "git::https://github.com/AeternaModules/azurerm_stream_analytics_job_schedule.git?ref=v4.80.0"
+  source                         = "git::https://github.com/AeternaModules/azurerm_stream_analytics_job_schedule.git?ref=v4.81.0"
   stream_analytics_job_schedules = local.stream_analytics_job_schedules
   depends_on                     = [module.stream_analytics_jobs]
 }
 
 module "stream_analytics_job_storage_accounts" {
-  source                                = "git::https://github.com/AeternaModules/azurerm_stream_analytics_job_storage_account.git?ref=v4.80.0"
+  source                                = "git::https://github.com/AeternaModules/azurerm_stream_analytics_job_storage_account.git?ref=v4.81.0"
   stream_analytics_job_storage_accounts = local.stream_analytics_job_storage_accounts
   depends_on                            = [module.stream_analytics_jobs]
 }
 
 module "stream_analytics_output_blobs" {
-  source                        = "git::https://github.com/AeternaModules/azurerm_stream_analytics_output_blob.git?ref=v4.80.0"
+  source                        = "git::https://github.com/AeternaModules/azurerm_stream_analytics_output_blob.git?ref=v4.81.0"
   stream_analytics_output_blobs = local.stream_analytics_output_blobs
   depends_on                    = [module.stream_analytics_jobs]
 }
 
 module "stream_analytics_output_cosmosdbs" {
-  source                            = "git::https://github.com/AeternaModules/azurerm_stream_analytics_output_cosmosdb.git?ref=v4.80.0"
+  source                            = "git::https://github.com/AeternaModules/azurerm_stream_analytics_output_cosmosdb.git?ref=v4.81.0"
   stream_analytics_output_cosmosdbs = local.stream_analytics_output_cosmosdbs
   depends_on                        = [module.stream_analytics_jobs]
 }
 
 module "stream_analytics_output_eventhubs" {
-  source                            = "git::https://github.com/AeternaModules/azurerm_stream_analytics_output_eventhub.git?ref=v4.80.0"
+  source                            = "git::https://github.com/AeternaModules/azurerm_stream_analytics_output_eventhub.git?ref=v4.81.0"
   stream_analytics_output_eventhubs = local.stream_analytics_output_eventhubs
   depends_on                        = [module.stream_analytics_jobs]
 }
 
 module "stream_analytics_output_functions" {
-  source                            = "git::https://github.com/AeternaModules/azurerm_stream_analytics_output_function.git?ref=v4.80.0"
+  source                            = "git::https://github.com/AeternaModules/azurerm_stream_analytics_output_function.git?ref=v4.81.0"
   stream_analytics_output_functions = local.stream_analytics_output_functions
   depends_on                        = [module.stream_analytics_jobs]
 }
 
 module "stream_analytics_output_mssqls" {
-  source                         = "git::https://github.com/AeternaModules/azurerm_stream_analytics_output_mssql.git?ref=v4.80.0"
+  source                         = "git::https://github.com/AeternaModules/azurerm_stream_analytics_output_mssql.git?ref=v4.81.0"
   stream_analytics_output_mssqls = local.stream_analytics_output_mssqls
   depends_on                     = [module.stream_analytics_jobs]
 }
 
 module "stream_analytics_output_powerbis" {
-  source                           = "git::https://github.com/AeternaModules/azurerm_stream_analytics_output_powerbi.git?ref=v4.80.0"
+  source                           = "git::https://github.com/AeternaModules/azurerm_stream_analytics_output_powerbi.git?ref=v4.81.0"
   stream_analytics_output_powerbis = local.stream_analytics_output_powerbis
   depends_on                       = [module.stream_analytics_jobs]
 }
 
 module "stream_analytics_output_servicebus_queues" {
-  source                                    = "git::https://github.com/AeternaModules/azurerm_stream_analytics_output_servicebus_queue.git?ref=v4.80.0"
+  source                                    = "git::https://github.com/AeternaModules/azurerm_stream_analytics_output_servicebus_queue.git?ref=v4.81.0"
   stream_analytics_output_servicebus_queues = local.stream_analytics_output_servicebus_queues
   depends_on                                = [module.stream_analytics_jobs]
 }
 
 module "stream_analytics_output_servicebus_topics" {
-  source                                    = "git::https://github.com/AeternaModules/azurerm_stream_analytics_output_servicebus_topic.git?ref=v4.80.0"
+  source                                    = "git::https://github.com/AeternaModules/azurerm_stream_analytics_output_servicebus_topic.git?ref=v4.81.0"
   stream_analytics_output_servicebus_topics = local.stream_analytics_output_servicebus_topics
   depends_on                                = [module.stream_analytics_jobs]
 }
 
 module "stream_analytics_output_synapses" {
-  source                           = "git::https://github.com/AeternaModules/azurerm_stream_analytics_output_synapse.git?ref=v4.80.0"
+  source                           = "git::https://github.com/AeternaModules/azurerm_stream_analytics_output_synapse.git?ref=v4.81.0"
   stream_analytics_output_synapses = local.stream_analytics_output_synapses
   depends_on                       = [module.stream_analytics_jobs]
 }
 
 module "stream_analytics_output_tables" {
-  source                         = "git::https://github.com/AeternaModules/azurerm_stream_analytics_output_table.git?ref=v4.80.0"
+  source                         = "git::https://github.com/AeternaModules/azurerm_stream_analytics_output_table.git?ref=v4.81.0"
   stream_analytics_output_tables = local.stream_analytics_output_tables
   depends_on                     = [module.stream_analytics_jobs]
 }
 
 module "stream_analytics_reference_input_blobs" {
-  source                                 = "git::https://github.com/AeternaModules/azurerm_stream_analytics_reference_input_blob.git?ref=v4.80.0"
+  source                                 = "git::https://github.com/AeternaModules/azurerm_stream_analytics_reference_input_blob.git?ref=v4.81.0"
   stream_analytics_reference_input_blobs = local.stream_analytics_reference_input_blobs
   depends_on                             = [module.stream_analytics_jobs]
 }
 
 module "stream_analytics_reference_input_mssqls" {
-  source                                  = "git::https://github.com/AeternaModules/azurerm_stream_analytics_reference_input_mssql.git?ref=v4.80.0"
+  source                                  = "git::https://github.com/AeternaModules/azurerm_stream_analytics_reference_input_mssql.git?ref=v4.81.0"
   stream_analytics_reference_input_mssqls = local.stream_analytics_reference_input_mssqls
   depends_on                              = [module.stream_analytics_jobs]
 }
 
 module "stream_analytics_stream_input_blobs" {
-  source                              = "git::https://github.com/AeternaModules/azurerm_stream_analytics_stream_input_blob.git?ref=v4.80.0"
+  source                              = "git::https://github.com/AeternaModules/azurerm_stream_analytics_stream_input_blob.git?ref=v4.81.0"
   stream_analytics_stream_input_blobs = local.stream_analytics_stream_input_blobs
   depends_on                          = [module.stream_analytics_jobs]
 }
 
 module "stream_analytics_stream_input_eventhubs" {
-  source                                  = "git::https://github.com/AeternaModules/azurerm_stream_analytics_stream_input_eventhub.git?ref=v4.80.0"
+  source                                  = "git::https://github.com/AeternaModules/azurerm_stream_analytics_stream_input_eventhub.git?ref=v4.81.0"
   stream_analytics_stream_input_eventhubs = local.stream_analytics_stream_input_eventhubs
   depends_on                              = [module.stream_analytics_jobs]
 }
 
 module "stream_analytics_stream_input_eventhub_v2s" {
-  source                                     = "git::https://github.com/AeternaModules/azurerm_stream_analytics_stream_input_eventhub_v2.git?ref=v4.80.0"
+  source                                     = "git::https://github.com/AeternaModules/azurerm_stream_analytics_stream_input_eventhub_v2.git?ref=v4.81.0"
   stream_analytics_stream_input_eventhub_v2s = local.stream_analytics_stream_input_eventhub_v2s
   depends_on                                 = [module.stream_analytics_jobs]
 }
 
 module "stream_analytics_stream_input_iothubs" {
-  source                                = "git::https://github.com/AeternaModules/azurerm_stream_analytics_stream_input_iothub.git?ref=v4.80.0"
+  source                                = "git::https://github.com/AeternaModules/azurerm_stream_analytics_stream_input_iothub.git?ref=v4.81.0"
   stream_analytics_stream_input_iothubs = local.stream_analytics_stream_input_iothubs
   depends_on                            = [module.stream_analytics_jobs]
 }
